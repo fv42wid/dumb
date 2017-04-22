849137429cdb42d902ed08917fadb31c9af8c0e9
Frank Vinci <fv42wid@yahoo.com>
2017-04-22 20:11:58 +0000

    likes work, add flash messages next

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :shits do 
     resources :comments, only: [:create, :destroy]
-    resource :like, only: [:create, :destroy]
+    resource :like, only: [:create, :destroy], module: :shits
   end
 
   root 'shits#index'
